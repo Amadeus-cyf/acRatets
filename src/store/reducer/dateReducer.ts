92dc56e8b0253fd2afd70cbd6fa0af1e8691bd16
Amadeus-cyf <amadeuscyf@gmail.com>
2021-08-29 17:24:13 +0800

    remove unused code

--- src/store/reducer/dateReducer.ts
+++
@@ -1,9 +0,0 @@
-import { getCurrentDate } from "../../utils/dateutil";
-import { DateActionType } from '../../interface/ActionType';
-
-export default function dateReducer(state = getCurrentDate(), action : DateActionType) {
-    switch(action.type) {
-        case "SWITCH_YEAR":
-            
-    }
-}
